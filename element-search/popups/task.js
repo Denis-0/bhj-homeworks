850f41b76e410f01a0058df3639189d632778230
Denis Kirilenko <kirilenko.dv@gmail.com>
2019-07-10 21:06:12 +0300

    first commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,11 +1,27 @@
+"use strict";
 
-let popup = document.getElementById('modal_main');
-let popupClose = document.querySelector('modal__close');
+const modal = document.getElementsByClassName('modal');
+modal.item(0).classList.add(`modal_active`);
 
-popup.onclick = function () {
-	
+const modalСlose = document.querySelectorAll('div.modal__content div.modal__close');
+
+const modalCloseLength = modalСlose.length;
+for (let i = 0; i < modalCloseLength; i++) {
+  modalСlose.item(i).onclick = function() {
+    modal.item(i).classList.remove('modal_active');
+
+    if (i == 0) {
+      let repeat = function() {
+        modal.item(0).classList.add('modal_active');
+      }
+      setTimeout(repeat, 1000);
+    }
+
+  }
 }
 
-popupClose.onclick = function () {
-	
-}
+const showSuccess = document.getElementsByClassName('show-success');  
+  showSuccess.item(0).onclick = function() {
+    modal.item(0).classList.remove('modal_active');
+    modal.item(1).classList.add('modal_active');
+  }
