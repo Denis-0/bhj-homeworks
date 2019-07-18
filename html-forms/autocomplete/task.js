48252ab8ceef969d02aaba0ad2e3736ae9e8588c
Denis Kirilenko <kirilenko.dv@gmail.com>
2019-07-18 22:20:40 +0300

    commit html-forms

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,19 +68,16 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    let selectFiltered = [];
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    for (let item of this.input) {
+      if ( item.text.includes(text) ) {
+        selectFiltered.push({
+          text: `${item.text}`,
+          value: `${item.value}`
+        });
       }
-    */
+    }
     return [
       {
         text: 'Чубакка',
